fdd3a39283608d631f19c19738f60a3254c02e81
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-07-18 14:39:49 -0400

    all done yay

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# simple-get-requests-ben-cody
+# Simple Get Request (Cody & Ben)
+This app allows a user to display random dog images (up to 50) or a single random image of the breed that they input.
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function getDogImage(num) {
   const url = 'https://dog.ceo/api/breeds/image/random/';
   fetch(url + num)
     .then(response => response.json())
-    .then(jsonData => extractData(jsonData))
     .then(data => renderImages(data))
     .catch(error => console.log(error));
 }
@@ -14,8 +13,8 @@ function getBreedImage(breed) {
   const url = `https://dog.ceo/api/breed/${breed}/images/random`;
   fetch(url)
     .then(response => response.json())
-    .then(jsonData => extractData(jsonData))
-    .then(messages => renderImages(messages));
+    .then(messages => renderImages(messages))
+    .catch(error => console.log(error));
 }
 
 function handleUserSubmit() {
@@ -28,28 +27,24 @@ function handleUserSubmit() {
   });
 }
 
-function renderImages(dataArr) {
-  if (dataArr[1] === 'error') {
+function renderImages(data) {
+  $('.dog-imgs').html('<h2>Dog Pics</h2>');
+  if (data.status === 'error') {
     $('.dog-imgs').html(`
     <h2>Error!</h2>
-    <p>${dataArr[0]}</p>
+    <p>${data.message}</p>
     <img src='https://www.petsuppliesplus.com/-/media/Images/PSP/Product%20Images/2017/03/29/20/59/750683200043_4.ashx?h=590?h=453' alt="empty dog cage">
   `);
-  } else if (typeof dataArr[0] === 'object') {
-    console.log(dataArr[0]);
-    $('.dog-imgs').html('<h2>Dog Pics</h2>');
-    dataArr[0].forEach(imgUrl => {
+  } else if (typeof data.message === 'object') {
+    console.log(data.message);
+    data.message.forEach(imgUrl => {
       $('.dog-imgs').append(`<img src="${imgUrl}" alt="dog image">`);
     });
   } else {
-    console.log(dataArr[0]);
-    $('.dog-imgs').append(`<img src="${dataArr[0]}" alt="dog image">`);
-  }
-}
+    console.log(data.message);
 
-function extractData(data) {
-  const { message, status } = data;
-  return [message, status];
+    $('.dog-imgs').append(`<img src="${data.message}" alt="dog image">`);
+  }
 }
 
 function handleBreedSubmit() {
